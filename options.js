38e998e7a60d43ee92250298c5c67b02e5e3cc0c
Ash Holland <ash@sorrel.sh>
2020-09-06 14:55:40 +0100

    Add unsynced debug mode setting

--- options.js
+++ options.js
@@ -1,9 +1,10 @@
-import {getRegularColour, setRegularColour, getIncognitoColour, setIncognitoColour} from './config.js'
+import {getRegularColour, setRegularColour, getIncognitoColour, setIncognitoColour, getDebugMode, setDebugMode} from './config.js'
 
 function loadOptions() {
   let status = document.getElementById("status")
   let colour = document.getElementById("colour")
   let incognitoColour = document.getElementById("incognito-colour")
+  let debugMode = document.getElementById("debug-mode")
   getRegularColour().then(val => {
     colour.value = val
   }).catch(error => {
@@ -14,18 +15,29 @@ function loadOptions() {
   }).catch(error => {
     status.textContent = `Couldn't load settings: ${error}`
   })
+  getDebugMode().then(val => {
+    debugMode.checked = val
+  }).catch(error => {
+    status.textContent = `Couldn't load settings: ${error}`
+  })
 }
 
 function saveOptions() {
   let status = document.getElementById("status")
   let colour = document.getElementById("colour")
   let incognitoColour = document.getElementById("incognito-colour")
+  let debugMode = document.getElementById("debug-mode")
   setRegularColour(colour.value).then(() => {
     status.textContent = ""
   }).catch(error => {
     status.textContent = `Couldn't save settings: ${error}`
   })
   setIncognitoColour(incognitoColour.value).then(() => {
+    status.textContent = ""
+  }).catch(error => {
+    status.textContent = `Couldn't save settings: ${error}`
+  })
+  setDebugMode(debugMode.checked).then(() => {
     status.textContent = ""
   }).catch(error => {
     status.textContent = `Couldn't save settings: ${error}`
